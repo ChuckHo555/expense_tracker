a1434d6b2cd1fc958a476da2a4cfe1ef136c6c1e
Chuck <chuckho555@gmail.com>
2025-04-16 22:21:50 -0400

    Added all variables and controllers for new expense

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -1,4 +1,8 @@
+import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+final formatter = DateFormat.yMd();
 
 class NewExpense extends StatefulWidget {
   const NewExpense({super.key});
@@ -12,12 +16,30 @@ class NewExpense extends StatefulWidget {
 class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
+  DateTime? _selectedDate;
+  Category _selectedCategory = Category.leisure;
 
+  //Controller for amount
   @override
   void dispose() {
     _titleController.dispose();
     _amountController.dispose();
+    //Make sure to dispose of it
     super.dispose();
+  }
+
+  void _presentDatePicker() async {
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year - 1, now.month, now.day);
+    final pickedDate = await showDatePicker(
+      context: context,
+      initialDate: now,
+      firstDate: firstDate,
+      lastDate: now,
+    );
+    setState(() {
+      _selectedDate = pickedDate;
+    });
   }
 
   @override
@@ -26,7 +48,6 @@ class _NewExpenseState extends State<NewExpense> {
       padding: const EdgeInsets.all(16),
       child: Column(
         children: [
-          // Title input
           Row(
             children: [
               Expanded(
@@ -34,13 +55,11 @@ class _NewExpenseState extends State<NewExpense> {
                   controller: _titleController,
                   maxLength: 50,
                   keyboardType: TextInputType.name,
-                  decoration: const InputDecoration(label: Text('Title')),
+                  decoration: InputDecoration(label: Text('Title')),
                 ),
               ),
             ],
           ),
-
-          // Amount input
           Row(
             children: [
               Expanded(
@@ -48,31 +67,66 @@ class _NewExpenseState extends State<NewExpense> {
                   controller: _amountController,
                   keyboardType: TextInputType.number,
                   decoration: const InputDecoration(
-                    label: Text('Amount'),
-                    prefixText: '\$ ',
+                    prefixText: '\$',
+                    label: Text("Amount"),
                   ),
+                ),
+              ),
+              const SizedBox(width: 16),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      _selectedDate == null
+                          ? 'Selected Date'
+                          : formatter.format(_selectedDate!),
+                    ),
+                    IconButton(
+                      onPressed: _presentDatePicker,
+                      icon: const Icon(Icons.calendar_month),
+                    ),
+                  ],
                 ),
               ),
             ],
           ),
-
-          // Buttons
           Row(
-            mainAxisAlignment: MainAxisAlignment.end,
             children: [
-              ElevatedButton(
-                onPressed: () {
-                  print('Title: ${_titleController.text}');
-                  print('Amount: ${_amountController.text}');
+              DropdownButton(
+                value: _selectedCategory,
+                items:
+                    Category.values
+                        .map(
+                          (category) => DropdownMenuItem(
+                            value: category,
+                            child: Text(category.name.toString().toUpperCase()),
+                          ),
+                        )
+                        .toList(),
+                onChanged: (value) {
+                  if (value == null) {
+                    return;
+                  }
+                  setState(() {
+                    _selectedCategory = value;
+                  });
                 },
-                child: const Text('Save Expense'),
               ),
-              const SizedBox(width: 12),
               ElevatedButton(
                 onPressed: () {
-                  Navigator.pop(context); // Closes the modal
+                  Navigator.pop(context);
                 },
-                child: const Text('Cancel'),
+                child: const Text("Cancel"),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  print(_titleController.text);
+                  print(_amountController.text);
+                  //print the amount data
+                },
+                child: Text('Save Expense'),
               ),
             ],
           ),
